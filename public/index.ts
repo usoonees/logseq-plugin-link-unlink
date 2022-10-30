bdcccc332229c71ee5280d82182c766db10cfdfe
usoon <usoon.xu@gmail.com>
2022-10-30 22:19:55 +0800

    highlight

--- public/index.ts
+++ public/index.ts
@@ -1,6 +1,17 @@
 import "@logseq/libs"
 
 const doc = parent.document
+
+async function getPageNames() {
+  const page = await logseq.Editor.getCurrentPage()
+  let pageNames = [page.name]
+  if(page.properties) {
+    pageNames = pageNames.concat(page.properties['alias'])
+  }
+
+  return pageNames
+}
+
 async function main() {
   logseq.provideStyle(`
     button.link-button {
@@ -8,6 +19,12 @@ async function main() {
       padding-left: 5px;
       padding-right: 5px;
     }
+
+    span.link-highlight {
+      backgroun-color: red;
+      background-color: yellow;
+    }
+
   `)
 
   logseq.Editor.registerSlashCommand("unlink", async (e) => {
@@ -25,6 +42,29 @@ async function main() {
       })
 
       blocks[i].querySelector('.inline').appendChild(linkButton)
+      const inline = blocks[i].querySelector('.inline')
+      const pageNames = await getPageNames()
+
+      const reStr = '(' + pageNames.join('|') + ')'
+      const re = new RegExp(reStr, "ig");
+      let child = inline.firstChild
+      let processChild = []
+
+      while (child) {
+        if (child.nodeType == 3) {
+          processChild.push(child)
+        }
+
+        child = child.nextSibling;
+      }
+
+      processChild.forEach(child => {
+        const text = child.textContent
+        let domText = text.replace(re, '<span class="link-highlight">$1</span>')
+        let newDom = document.createElement("span");
+        newDom.innerHTML = domText
+        inline.replaceChild(newDom, child)
+      })
     }
   });
 }
