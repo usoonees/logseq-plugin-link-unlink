551721c7c00723b1596f6b8ca8fb61e2179a2dc9
usoon <usoon.xu@gmail.com>
2023-01-17 20:50:06 +0800

    feat: highlight tag and ref in linked references

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ A plugin for logseq to highlight the keyword in unlinked reference, and provide 
 #### Screenshot
 ![](image/screenshot.png)
 
+You can also highlight the refs and tags in linked references (need to enable it in the settings).
+![](image/linked.png) 
+
 #### Demo
 ![Demo](image/demo.gif)
 
--- public/index.ts
+++ public/index.ts
@@ -1,16 +1,119 @@
 import "@logseq/libs"
 
 const doc = parent.document
-const highlightClass = 'unlink-highlight'
+const highlightClass = "unlink-highlight"
 
 async function getPageNames() {
   const page = await logseq.Editor.getCurrentPage()
   let pageNames = [page.name]
-  if(page.properties && page.properties['alias']) {
-    pageNames = pageNames.concat(page.properties['alias'])
+  if (page.properties && page.properties["alias"]) {
+    pageNames = pageNames.concat(page.properties["alias"])
   }
 
   return pageNames
+}
+
+let prePageNames = []
+async function highlightLinked() {
+  const curPageNames = await getPageNames()
+
+  // references
+  if (logseq.settings.highlightLinkedRefs) {
+    highlightLinkRef(prePageNames, curPageNames)
+  } else {
+    unHighlightRef(prePageNames)
+  }
+
+  // tags
+  if (logseq.settings.highlightLinkedTags) {
+    highlighTagRef(prePageNames, curPageNames)
+  } else {
+    unHighlightTag(prePageNames)
+  }
+
+  prePageNames = curPageNames
+  
+}
+
+function unHighlightRef(preNames) {
+  const unsetSelector = preNames.map((name) => `.page-linked .page-ref[data-ref="${name}"]`).join(",")
+  const unsetDarkSelector = preNames.map((name) => `.dark .page-linked .page-ref[data-ref="${name}"]`).join(",")
+  logseq.provideStyle(`
+    ${unsetSelector} {
+      background-color: unset;
+    }
+
+    ${unsetDarkSelector} {
+      background-color: unset;
+    }
+  `)
+}
+
+function highlightLinkRef(preNames, curNames) {
+  const unsetSelector = preNames.map((name) => `.page-linked .page-ref[data-ref="${name}"]`).join(",")
+  const unsetDarkSelector = preNames.map((name) => `.dark .page-linked .page-ref[data-ref="${name}"]`).join(",")
+  const setSelector = curNames.map((name) => `.page-linked .page-ref[data-ref="${name}"]`).join(",")
+  const setDarkSelector = curNames.map((name) => `.dark .page-linked .page-ref[data-ref="${name}"]`).join(",")
+  logseq.provideStyle(`
+    ${unsetSelector} {
+      background-color: unset;
+    }
+
+    ${unsetDarkSelector} {
+      background-color: unset;
+    }
+
+    ${setSelector} {
+      background-color: yellow;
+    }
+
+    ${setDarkSelector} {
+      background-color:  #ffff0030;
+    }
+  `)
+}
+
+function unHighlightTag(preNames) {
+  const unsetTagSelector = preNames.map((name) => `.page-linked a.tag[data-ref="${name}"]`).join(",")
+  const unsetTagDarkSelector = preNames.map((name) => `.dark .page-linked a.tag[data-ref="${name}"]`).join(",")
+  logseq.provideStyle(`
+  ${unsetTagSelector} {
+    background-color: unset;
+    color: unset;
+  }
+
+  ${unsetTagDarkSelector} {
+    background-color: unset;
+    color: unset;
+  }`)
+}
+
+function highlighTagRef(preNames, curNames) {
+  const unsetTagSelector = preNames.map((name) => `.page-linked a.tag[data-ref="${name}"]`).join(",")
+  const unsetTagDarkSelector = preNames.map((name) => `.dark .page-linked a.tag[data-ref="${name}"]`).join(",")
+  const setTagSelector = curNames.map((name) => `.page-linked a.tag[data-ref="${name}"]`).join(",")
+  const setTagDarkSelector = curNames.map((name) => `.dark .page-linked a.tag[data-ref="${name}"]`).join(",")
+  logseq.provideStyle(`
+  ${unsetTagSelector} {
+    background-color: unset;
+    color: unset;
+  }
+
+  ${unsetTagDarkSelector} {
+    background-color: unset;
+    color: unset;
+  }
+
+  ${setTagSelector} {
+    background-color: yellow;
+    color: #484576;
+  }
+
+  ${setTagDarkSelector} {
+    background-color:  #ffff0030;
+    color: #ebdddd;
+  }
+`)
 }
 
 async function main() {
@@ -44,67 +147,94 @@ async function main() {
   }
   `)
 
-  let unlinkObserver, unlinkedRefsContainer;
+  logseq.useSettingsSchema([
+    {
+      key: "highlightLinkedRefs",
+      title: "Whether to highlight references in linked references",
+      type: "boolean",
+      default: false, // default to false
+      description: "",
+    },
+    {
+      key: "highlightLinkedTags",
+      title: "Whether to highlight tag in linked references",
+      type: "boolean",
+      default: false, // default to false
+      description: "",
+    },
+  ])
+
+
+  logseq.onSettingsChanged(async () => {
+    await highlightLinked()
+  })
+
+  logseq.App.onRouteChanged(() => {
+    setTimeout(async () => {
+      await highlightLinked()
+    }, 500)
+  })
+
+  let unlinkObserver, unlinkedRefsContainer
 
   const unlinkCallback = function (mutationsList, observer) {
     for (let i = 0; i < mutationsList.length; i++) {
-      const addedNode = mutationsList[i].addedNodes[0];
+      const addedNode = mutationsList[i].addedNodes[0]
       if (addedNode && addedNode.childNodes.length) {
-        const blocks = addedNode.querySelectorAll('.block-content')
+        const blocks = addedNode.querySelectorAll(".block-content")
         if (blocks.length) {
           unlinkObserver.disconnect()
-          for (let i=0; i < blocks.length; i++) {
+          for (let i = 0; i < blocks.length; i++) {
             addHighlight(blocks[i])
           }
-          unlinkObserver.observe(unlinkedRefsContainer, obConfig);
+          unlinkObserver.observe(unlinkedRefsContainer, obConfig)
         }
       }
     }
-  };
+  }
 
   const obConfig = {
     childList: true,
     subtree: true,
-  };
-  unlinkObserver = new MutationObserver(unlinkCallback);
+  }
+  unlinkObserver = new MutationObserver(unlinkCallback)
 
   function addObserverIfDesiredNodeAvailable() {
-    unlinkedRefsContainer = doc.querySelector(".page>div:nth-last-child(1) .references");
-    if(!unlinkedRefsContainer) {
-        setTimeout(addObserverIfDesiredNodeAvailable, 200);
-        return;
+    unlinkedRefsContainer = doc.querySelector(".page-unlinked .references")
+    if (!unlinkedRefsContainer) {
+      setTimeout(addObserverIfDesiredNodeAvailable, 200)
+      return
     }
-    unlinkObserver.observe(unlinkedRefsContainer, obConfig);
+    unlinkObserver.observe(unlinkedRefsContainer, obConfig)
   }
-  addObserverIfDesiredNodeAvailable();
+  addObserverIfDesiredNodeAvailable()
 
   logseq.App.onRouteChanged(() => {
     setTimeout(() => {
-      addObserverIfDesiredNodeAvailable();
+      addObserverIfDesiredNodeAvailable()
     }, 50) // wait for page load, otherwise would observer the previous page
-  });
+  })
 
   logseq.beforeunload(async () => {
     unlinkObserver.disconnect()
   })
-
 }
 
-const contentSelector = '.inline, .is-paragraph, h1, h2, h3, h4, h5, h6'
+const contentSelector = ".inline, .is-paragraph, h1, h2, h3, h4, h5, h6"
 function addButton(blockEl, pageNames, isPureText) {
-  let linkButton = blockEl.querySelector('.link-button')
+  let linkButton = blockEl.querySelector(".link-button")
   if (linkButton) {
     return
   }
-  const blockID = blockEl.getAttribute('blockid')
-  linkButton = doc.createElement("button");
-  linkButton.setAttribute("class", "link-button");
-  linkButton.innerHTML = "link";
+  const blockID = blockEl.getAttribute("blockid")
+  linkButton = doc.createElement("button")
+  linkButton.setAttribute("class", "link-button")
+  linkButton.innerHTML = "link"
   linkButton.addEventListener("click", async (e) => {
     const block = await logseq.Editor.getBlock(blockID)
     const content = block.content
-    const reStr = '(' + pageNames.join('|') + ')'
-    const re = new RegExp(reStr, "ig");
+    const reStr = "(" + pageNames.join("|") + ")"
+    const re = new RegExp(reStr, "ig")
 
     /*
        page = cu
@@ -112,73 +242,72 @@ function addButton(blockEl, pageNames, isPureText) {
       '[[cu]] #focus #f/ocus #cu [[cu]]s fo[[cu]]s [[cu]] [[focus]]'
     */
     const newContent = content.replace(re, (match, _, i) => {
-      while(i>=0) {
-          if(/\s/.test(content[i]) || content[i] === ']') {
-              break
-          } else if (content[i] == '[' || content[i] == '#') {
-              return match
-          }
-          i -= 1;
+      while (i >= 0) {
+        if (/\s/.test(content[i]) || content[i] === "]") {
+          break
+        } else if (content[i] == "[" || content[i] == "#") {
+          return match
+        }
+        i -= 1
       }
 
       return `[[${match}]]`
-  })
+    })
     console.log("unlinked content: ", content, pageNames)
     console.log("linked content: ", newContent)
 
     await logseq.Editor.updateBlock(blockID, newContent)
 
-    if(!isPureText) { // sometimes header and paragraph would cause block render error
+    if (!isPureText) {
+      // sometimes header and paragraph would cause block render error
       await logseq.Editor.editBlock(blockID)
       logseq.Editor.exitEditingMode()
     }
 
     let highlights = blockEl.querySelectorAll(`.${highlightClass}`)
-    for (let i=0; i < highlights.length; i++) {
+    for (let i = 0; i < highlights.length; i++) {
       // highlights[i].style.display = 'none'
       highlights[i].remove()
     }
-    linkButton.style.display = 'none'
+    linkButton.style.display = "none"
   })
 
   blockEl.querySelector(contentSelector).appendChild(linkButton)
 }
 
-
 async function addHighlight(blockEl) {
   const contentElements = blockEl.querySelectorAll(contentSelector)
   const pageNames = await getPageNames()
-  const reStr = '(' + pageNames.join('|') + ')'
-  const re = new RegExp(reStr, "ig");
+  const reStr = "(" + pageNames.join("|") + ")"
+  const re = new RegExp(reStr, "ig")
 
-  contentElements.forEach(content => {
-
+  contentElements.forEach((content) => {
     let child = content.firstChild
-    let pureText = []  // pure text in first level of inline
-    let formatText = [];  // bold, italic, mark text in second level of inline
+    let pureText = [] // pure text in first level of inline
+    let formatText = [] // bold, italic, mark text in second level of inline
 
     while (child) {
-      if (child.nodeType == 3 ) {
+      if (child.nodeType == 3) {
         pureText.push(child)
       }
 
-      if(['B', 'I', 'EM', 'STRONG', 'MARK', 'DEL'].includes(child.tagName)) {
+      if (["B", "I", "EM", "STRONG", "MARK", "DEL"].includes(child.tagName)) {
         formatText.push({
-          "parent": child,
-          "children": []
+          parent: child,
+          children: [],
         })
       }
 
-      child = child.nextSibling;
+      child = child.nextSibling
     }
 
-    for(let i=0; i < formatText.length; i++) {
+    for (let i = 0; i < formatText.length; i++) {
       child = formatText[i]["parent"].firstChild
       while (child) {
-        if (child.nodeType == 3 ) {
+        if (child.nodeType == 3) {
           formatText[i]["children"].push(child)
         }
-        child = child.nextSibling;
+        child = child.nextSibling
       }
     }
 
@@ -188,22 +317,25 @@ async function addHighlight(blockEl) {
         const isPureText = content.tagName == "SPAN"
         addButton(blockEl, pageNames, isPureText)
       }
-      let domText = text.replace(re, `<span class="${highlightClass}">$1</span>`)
-      let newDom = document.createElement("span");
+      let domText = text.replace(
+        re,
+        `<span class="${highlightClass}">$1</span>`,
+      )
+      let newDom = document.createElement("span")
       newDom.innerHTML = domText
       return newDom
     }
 
-    pureText.forEach(child => {
+    pureText.forEach((child) => {
       content.replaceChild(newHighlightNode(child), child)
     })
 
-    formatText.forEach(boldNode => {
-      boldNode["children"].forEach(c => {
+    formatText.forEach((boldNode) => {
+      boldNode["children"].forEach((c) => {
         boldNode["parent"].replaceChild(newHighlightNode(c), c)
       })
     })
-  });
+  })
 }
 
 logseq.ready(main).catch(console.error)
